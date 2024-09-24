2943504c5af07ee6a98af018f956a36d82065a13
vrbdas <vrbdas@gmail.com>
2024-09-24 10:47:36 +0500

    final commit

--- src/components/AppComments.vue
+++ src/components/AppComments.vue
@@ -4,11 +4,13 @@ import { ref } from 'vue';
 
 const comments = ref([]);
 const errorMessage = ref('');
+const loading = ref(false);
 const loaded = ref(false);
 
 async function load() {
   try {
-    const {data} = await axios.get('https://vue-couseworktwo-default-rtdb.asia-southeast1.firebasedatabase.app/comments.json');
+    loading.value = true;
+    const { data } = await axios.get('https://vue-couseworktwo-default-rtdb.asia-southeast1.firebasedatabase.app/comments.json');
     comments.value = await data[Object.keys(data)[0]];
     errorMessage.value = '';
     loaded.value = true;
@@ -16,40 +18,51 @@ async function load() {
   catch {
     errorMessage.value = 'Network error';
   }
+  finally {
+    loading.value = false;
+  }
 }
 </script>
 
 <template>
-  <div v-if="comments.length === 0 && loaded === false" class="comments__btn">
+  <div class="comments center" v-if="loading === true">
+    <span class="loader" ></span>
+  </div>
+  <div v-else class="comments">
+    <div v-if="comments.length === 0 && loaded === false" class="comments__btn">
       <button class="btn btn_dark" @click="load">Load comments</button>
       <span v-if="errorMessage" class="comments__error">{{ errorMessage }}</span>
-  </div>
-  <div v-else-if="comments.length === 0 && loaded === true" class="card comments">
-    No comments
-  </div>
-  <div v-else class="card comments">
-    <h2 class="comments__title">Comments</h2>
-    <div class="comment" v-for="comment in comments" :key="comment">
-      <div class="comment__bold">{{ comment.email }}</div>
-      <div>{{ comment.commentary }}</div>
+    </div>
+    <div v-else-if="comments.length === 0 && loaded === true" class="card">
+      No comments
+    </div>
+    <div v-else class="card">
+      <h3 class="comments__title">Comments</h3>
+      <div class="comment" v-for="comment in comments" :key="comment">
+        <div class="comment__bold">{{ comment.email }}</div>
+        <div>{{ comment.commentary }}</div>
+      </div>
     </div>
   </div>
+
 </template>
 
 <style scoped lang='scss'>
 .comments {
   grid-column: span 3;
-  gap: 30px;
+
   &__title {
-    font-weight: 600;
+    font-weight: 400;
     font-size: 2rem;
     margin: 0 auto;
   }
+
   &__btn {
     display: flex;
     align-items: center;
     gap: 20px;
   }
+
   &__error {
     color: red;
   }
@@ -67,7 +80,7 @@ async function load() {
 
   &::after {
     content: '';
-    border-bottom: 1px solid rgb(209, 209, 209);
+    border-bottom: 1px solid rgb(201, 201, 201);
     position: absolute;
     bottom: -15px;
     left: 0;
@@ -79,6 +92,58 @@ async function load() {
       display: none;
     }
   }
+}
 
+.loader {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+  border: 3px solid;
+  border-color: #FFF #FFF transparent;
+  animation: rotation 1s linear infinite;
+
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    border: 3px solid;
+    border-color: transparent #BDA98B #BDA98B;
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    animation: rotationBack 0.5s linear infinite;
+    transform-origin: center center;
+  }
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes rotationBack {
+  0% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(-360deg);
+  }
 }
 </style>
--- src/components/AppResume.vue
+++ src/components/AppResume.vue
@@ -5,32 +5,33 @@ const props = defineProps(['resume']);
 
 <template>
   <div class="card resume">
-    <div v-for="(item, i) in resume" :key="i">
-      <h2 v-if="item.blockType === 'title'">{{ item.blockValue }}</h2>
-      <h3 v-if="item.blockType === 'subtitle'">{{ item.blockValue }}</h3>
-      <div class="photo" v-if="item.blockType === 'photo'"><img :src="item.blockValue"></div>
-      <p v-if="item.blockType === 'text'">{{ item.blockValue }}</p>
+    <div v-if="resume.length === 0">
+      <h3 class="resume__subtitle">Add first block to see the result</h3>
+    </div>
+    <div v-else>
+      <div v-for="(item, i) in resume" :key="i">
+        <h2 v-if="item.blockType === 'title'" class="resume__title">{{ item.blockValue }}</h2>
+        <h3 class="resume__subtitle" v-if="item.blockType === 'subtitle'">{{ item.blockValue }}</h3>
+        <div class="photo" v-if="item.blockType === 'photo'"><img :src="item.blockValue"></div>
+        <p v-if="item.blockType === 'text'">{{ item.blockValue }}</p>
+      </div>
     </div>
   </div>
 </template>
-
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque saepe beatae omnis, laudantium id mollitia nam quod magnam. Consectetur quibusdam in cumque perferendis fugiat numquam quo cupiditate eveniet tenetur ea?
 <style scoped lang='scss'>
 .resume {
   grid-column: span 2;
   gap: 10px;
-}
-
-h2 {
-  font-weight: 800;
-  font-size: 3rem;
-  margin: 0;
-}
-
-h3 {
-  font-weight: 600;
-  font-size: 2rem;
-  margin: 0;
+  &__title {
+    font-weight: 600;
+    font-size: 3rem;
+    margin: 0;
+  }
+  &__subtitle {
+    font-weight: 400;
+    font-size: 2rem;
+    margin: 0;
+  }
 }
 
 p {
@@ -41,6 +42,7 @@ p {
   width: 200px;
   height: 200px;
   margin: 0 auto;
+
   img {
     width: 100%;
     height: 100%;
